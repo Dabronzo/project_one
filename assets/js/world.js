a5e485ce25324bdc529f95401f8dad25c65fbb3f
Dabronzo <ayrtonterceiro@gmail.com>
2022-01-11 14:43:32 +0000

    Added image for test room and set image rendering to pixelated

--- assets/js/world.js
+++ assets/js/world.js
@@ -6,6 +6,10 @@ class World {
         this.ctx = this.canvas.getContext("2d");
     }
     init() {
-        console.log("Hello from the world", this);
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image, 0, 0)
+        };
+        image.src = "/assets/images/demo_lower.png";
     }
 }
